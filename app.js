8c6b8141d0247f69f9ff63ac63958daa74b870fe
warnerfleming <fleming14@usf.edu>
2024-10-27 22:55:10 -0400

    Add canvas setup and event handling logic.

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+//Task 2
 
+const canvas =document.getElementById(`canvas`)
+const drawingContext = canvas.getContext(`2d`)
+let color = document.getElementById("colorselect").value
+let toolSelect = `line`
+
+document.getElementById("tool").forEach(radio =>{
+    radio.addEventListener("change", (event)=> {
+        toolSelect = event.target.value 
+    })
+}    
+)
